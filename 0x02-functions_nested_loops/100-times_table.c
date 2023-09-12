1280dae4fccfc6f5ebccae630eefd2e758f3af41
mabelorji41 <mabelorji74@gmail.com>
2023-09-12 12:59:29 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the times table of the input, starting with 0
+ * @n: The value of the times table to be printed
+ */
+
+void print-times-table(int n)
+{
+	int num, multi, prod
+
+		if (n >= 0 && n <= 15)
+		{
+			for (num = 0, num <= n, num++)
+			{
+				_putchar['0']
+
+					for (mult = 1; mult <= n, mult ++)
+					{
+						_putchar(',');
+						_putchar(',');
+						
+						prod "num" mult
